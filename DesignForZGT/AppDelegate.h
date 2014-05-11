12979ac2940a6e040f8414556030ca9feb60a0ef
liuxiaodan <dbuyer@liuxiaodan.local>
2014-05-11 19:32:39 +0800

    Initial Commit

---
+++ DesignForZGT/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DesignForZGT
+//
+//  Created by liuxiaodan on 14-5-11.
+//  Copyright (c) 2014年 liuxiaodan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
